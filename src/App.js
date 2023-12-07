5beb518574473dcc54dfc694dc276ffc7735a5be
Takayuki <6015abc@gmail.com>
2023-12-08 01:21:37 +0900

    tailwind.cssをインストール実施

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      初期表示確認
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
