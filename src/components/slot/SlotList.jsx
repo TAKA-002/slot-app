24d76354c58bb7452719e7a3552e96528ba6da9f
Takayuki Miura <6015abc@gmail.com>
2024-01-07 07:58:03 +0900

    リストのoverflowを設定。

--- src/components/slot/SlotList.jsx
+++ src/components/slot/SlotList.jsx
@@ -1,17 +1,17 @@
 export default function SlotList({ lists, visibleIndex }) {
   return (
-    <ul className="w-96 h-[76px] relative z-[1] border-4 border-[#CF9D19] rounded-md px-8 py-4 overflow-scroll hidden-scrollbar bg-white ">
+    <ul className="w-96 h-[76px] relative z-[1] border-4 border-[#CF9D19] rounded-md px-8 py-4 overflow-x-scroll hidden-scrollbar bg-white ">
       {lists.map((item, index) => {
         if (index === visibleIndex) {
           return (
-            <li className="text-3xl font-sans" key={index}>
+            <li className="text-3xl font-sans whitespace-nowrap" key={index}>
               {item}
             </li>
           );
         } else {
           return (
             <li
-              className="text-3xl font-sans"
+              className="text-3xl font-sans whitespace-nowrap"
               style={{ display: "none" }}
               key={index}
             >
