c3aa320cac1640cb257b4ed325c48b2c681d3253
Takayuki Miura <6015abc@gmail.com>
2024-01-10 02:35:21 +0900

    formタグでボタンを作成

--- src/Member.js
+++ src/Member.js
@@ -14,7 +14,13 @@ export default function Member({ lists }) {
       <div>
         <div className="flex items-center justify-between w-full sm:w-[360px] mx-auto mt-4 px-4 sm:px-0">
           <h1 className="text-lg">スロットメンバー</h1>
-          <button class="px-4 py-2 text-base text-bold text-white bg-[#E82133] rounded-xl">確定ボタン</button>
+          <form className="" action="">
+            <input
+              class="px-4 py-2 text-base text-bold text-white bg-[#E82133] rounded-xl"
+              type="submit"
+              value="確定ボタン"
+            />
+          </form>
         </div>
         <List lists={lists} />
       </div>
