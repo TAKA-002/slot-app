3f4e429ce78fdf5c3881464dfd27b8e3db6ef17b
Takayuki Miura <6015abc@gmail.com>
2024-01-11 02:15:41 +0900

    リストのスタイリング

--- src/Member.js
+++ src/Member.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import MainHeader from './components/MainHeader.jsx';
 import Nav from './components/Nav.jsx';
-import List from './components/member/List.jsx';
+import MemberContainer from './components/member/MemberContainer.jsx';
 import MainFooter from './components/MainFooter.jsx';
 
 export default function Member({ lists }) {
@@ -11,20 +11,7 @@ export default function Member({ lists }) {
       <MainHeader />
       <Nav />
 
-      <div>
-        <div className="flex items-center justify-between w-full sm:w-[360px] mx-auto mt-4 px-4 sm:px-0">
-          <h1 className="text-lg">スロットメンバー</h1>
-          <form className="" action="">
-            <input
-              class="px-4 py-2 text-base text-bold text-white bg-[#E82133] rounded-xl"
-              type="submit"
-              value="確定ボタン"
-            />
-          </form>
-        </div>
-        <List lists={lists} />
-      </div>
-
+      <MemberContainer lists={lists} />
       <MainFooter />
     </>
   );
--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -5,7 +5,7 @@ import Logo from '../assets/img/webstaff.png';
 
 export default function MainHeader() {
   return (
-    <nav className="bg-[#E82133] opacity-90 py-2 md:py-4">
+    <header className="bg-[#E82133] opacity-90 py-2 md:py-4">
       <div className="container px-4 mx-auto md:flex md:items-center">
         <div className="w-full flex justify-between items-center">
           <Link to="/" className="w-[100px] sm:w-[200px] h-auto font-bold text-xl text-white">
@@ -16,6 +16,6 @@ export default function MainHeader() {
           </Link>
         </div>
       </div>
-    </nav>
+    </header>
   );
 }
