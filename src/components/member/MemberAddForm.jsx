a55b71463fd96cbda397d13889c54ffdf6d21bf5
Takayuki Miura <6015abc@gmail.com>
2024-01-15 01:36:37 +0900

    フォームからdata.jsonを出力するところまでできた

---
+++ src/components/member/MemberAddForm.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+
+export default function MemberAddForm({ formData, setFormData }) {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8888/slot-app/api/index.php', formData);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form className="relative z-[2] max-w-md mx-auto" onSubmit={handleSubmit}>
+      <div className="mb-5 flex gap-x-4">
+        <input
+          type="text"
+          name="name"
+          className="flex-1 bg-green-50 border border-green-500 text-green-900 placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 p-2.5"
+          placeholder="項目を記入してください。"
+          onChange={handleChange}
+        />
+        <button
+          type="submit"
+          className="py-2 px-8 font-bold text-red-900 rounded-lg border-[2px] border-red-500 bg-red-50 active:translate-y-2"
+        >
+          追加
+        </button>
+      </div>
+    </form>
+  );
+}
--- src/components/member/MemberContainer.jsx
+++ src/components/member/MemberContainer.jsx
@@ -1,24 +1,8 @@
-import axios from 'axios';
-
 import MemberListWrapper from './MemberListWrapper';
 import MemberDecoration from './MemberDecoration';
+import MemberAddForm from './MemberAddForm';
 
 export default function MemberContainer({ lists, formData, setFormData }) {
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post('http://localhost:8888/slot-app/api/index.php', formData);
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
   return (
     <section
       className="relative py-8 px-2 sm:px-0 bg-paripi min-h-[1300px]"
@@ -30,20 +14,7 @@ export default function MemberContainer({ lists, formData, setFormData }) {
         backgroundPosition: 'bottom',
       }}
     >
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" name="name" onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input type="text" name="email" onChange={handleChange} />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-
+      <MemberAddForm formData={formData} setFormData={setFormData} />
       <MemberDecoration />
       <MemberListWrapper lists={lists} />
     </section>
