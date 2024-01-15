ab415df5a1b3d302b67ee9a24bfd89d02ba6afed
Takayuki Miura <6015abc@gmail.com>
2024-01-16 01:03:29 +0900

    データを再作成。

--- src/components/member/MemberAddForm.jsx
+++ src/components/member/MemberAddForm.jsx
@@ -30,10 +30,12 @@ export default function MemberAddForm({ lists, formData, setFormData }) {
           className="flex-1 bg-green-50 border border-green-500 text-green-900 placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 p-2.5"
           placeholder="項目を記入してください。"
           onChange={handleChange}
+          tabIndex="1"
         />
         <button
           type="submit"
           className="py-2 px-8 font-bold text-red-900 rounded-lg border-[2px] border-red-500 bg-red-50 active:translate-y-2"
+          tabIndex="2"
         >
           追加
         </button>
